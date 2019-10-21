49a92e8903832db65b344f83fe2337fa5d01d192
x77lin <x77lin@nettop169.cs.uwaterloo.ca>
2019-10-20 21:30:32 -0400

    Fix idle task using IO server.

--- src/server/idle.c
+++ src/server/idle.c
@@ -1,25 +1,30 @@
 #include <hardware/syscon.h>
 #include <server/idle.h>
+#include <server/io.h>
+#include <user/io.h>
+#include <user/name.h>
 #include <user/tasks.h>
 #include <utils/bwio.h>
 
 static void idle_task() {
+    int io_server_id = WhoIs(IO_SERVER_NAME);
+    if (io_server_id < 0) {
+        io_server_id = CreateIOServer(3000, 3000, 3000);
+    }
+
+    const char *save_cursor = "\033[s";
+    const char *retn_cursor = "\033[u";
+    const char *clear_line  = "\033[K";
+    const char *idle_rate_title_position = "\033[1;1H";
+    const char *idle_rate_value_position = "\033[1;16H";
+
     syscon_lock(OFF);
     syscon_config(SHENA_MASK);
-    // Print Title
-    /*bwprintf(COM2, "\033[s");*/
-    /*bwprintf(COM2, "\033[1;1H");*/
-    /*bwprintf(COM2, "CPU Idle Rate: ");*/
-    /*bwprintf(COM2, "\033[u");*/
+    Printf(io_server_id, COM2, "%s%sCPU Idle Rate: %s", save_cursor, idle_rate_title_position, retn_cursor);
     Yield();
     for (;;) {
         syscon_halt();
-        // Print CPU usage
-        /*bwprintf(COM2, "\033[s");*/
-            /*bwprintf(COM2, "\033[1;16H");*/
-            /*bwprintf(COM2, "\033[K");*/
-            /*bwprintf(COM2, "%d%%", MyCpuUsage());*/
-        /*bwprintf(COM2, "\033[u");*/
+        Printf(io_server_id, COM2, "%s%s%d%%%s", save_cursor, idle_rate_value_position, clear_line, MyCpuUsage(), retn_cursor);
     }
     Exit();
 }
