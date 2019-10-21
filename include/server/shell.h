de71e5d8136a0da7168439288f835e8b36ee032e
Charles Gao <c39gao@uwaterloo.ca>
2019-10-20 22:26:03 -0400

    Add clock to shell

--- include/server/shell.h
+++ include/server/shell.h
@@ -2,12 +2,22 @@
 #define __SERVER_SHELL_H__
 
 #define SENSOR_READ_INTERVAL 100
+#define CLOCK_PRECISION 100
+
+#define SHELL_PRIORITY 3000
 
 #include <stdint.h>
+
+
+typedef struct {
+    uint32_t minute;
+    uint32_t second;
+    uint32_t decisecond;
+} ShellClock;
 
 /**
  * Creates a Shell Server
  */
-void CreateShellServer(uint32_t priority);
+int CreateShellServer(uint32_t priority);
 
 #endif /*__SERVER_SHELL_H__*/
--- src/server/shell.c
+++ src/server/shell.c
@@ -21,7 +21,40 @@ static void shell_keyboard_task() {
     Exit();
 }
 
+static void shell_clock_init(ShellClock *shellclock) {
+    shellclock->minute = 0;
+    shellclock->second = 0;
+    shellclock->decisecond = 0;
+}
+
+static void shell_clock_update(ShellClock *shellclock) {
+    if (shellclock->decisecond < 9) {
+        shellclock->decisecond++;
+        return;
+    }
+    shellclock->decisecond = 0;
+    if (shellclock->second < 59) {
+        shellclock->second++;
+        return;
+    }
+    shellclock->second = 0;
+    shellclock->minute++;
+}
+
+static void shell_clock_display(int iotid, ShellClock *shellclock) {
+    Printf(iotid, COM2, "%d:%d:%d\n\r", shellclock->minute, shellclock->second, shellclock->decisecond);
+}
+
 static void shell_clock_task() {
+    ShellClock shellclock;
+    shell_clock_init(&shellclock);
+    int clocktid = WhoIs(CLOCK_SERVER_NAME);
+    int iotid = WhoIs(IO_SERVER_NAME);
+    for (;;) {
+        Delay(clocktid, CLOCK_PRECISION/10);
+        shell_clock_update(&shellclock);
+        shell_clock_display(iotid, &shellclock);
+    }
     Exit();
 }
 
@@ -41,10 +74,14 @@ static void shell_sensor_task() {
     Exit();
 }
 
+void shell_server_root_task() {
+    Create(SHELL_PRIORITY, &shell_parser_task);
+    Create(SHELL_PRIORITY, &shell_keyboard_task);
+    Create(SHELL_PRIORITY, &shell_clock_task);
+    Create(SHELL_PRIORITY, &shell_sensor_task);
+    Exit();
+}
 
-void CreateShellServer(uint32_t priority) {
-    Create(priority, &shell_parser_task);
-    Create(priority, &shell_keyboard_task);
-    Create(priority, &shell_clock_task);
-    Create(priority, &shell_sensor_task);
+int CreateShellServer(uint32_t priority) {
+    return Create(priority, &shell_server_root_task);
 }
