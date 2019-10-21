a932214686da13a6dfa780e19b26118dec50144b
Charles Gao <c39gao@uwaterloo.ca>
2019-10-21 00:17:39 -0400

    Add format constant to shell

--- include/server/shell.h
+++ include/server/shell.h
@@ -8,6 +8,14 @@
 
 #include <stdint.h>
 
+#define LINE_TIME         2
+#define LINE_SWITCH_TITLE 3
+#define LINE_SWITCH_START 5
+#define LINE_SENSOR_TITLE 8
+#define LINE_SENSOR_START 9
+#define LINE_TERMINAL    16
+#define LINE_DEBUG       17
+#define CMD_BUFFER_SIZE  24
 
 typedef struct {
     uint32_t minute;
