7def24eef4c1a049d72fd5c86be94ae2106a19c5
Charles Gao <c39gao@uwaterloo.ca>
2019-10-20 21:36:43 -0400

    Add sensor to shell

--- include/server/shell.h
+++ include/server/shell.h
@@ -1,11 +1,13 @@
 #ifndef __SERVER_SHELL_H__
 #define __SERVER_SHELL_H__
 
+#define SENSOR_READ_INTERVAL 100
+
 #include <stdint.h>
 
 /**
  * Creates a Shell Server
  */
-int CreateShellServer(uint32_t priority);
+void CreateShellServer(uint32_t priority);
 
 #endif /*__SERVER_SHELL_H__*/
--- src/server/shell.c
+++ src/server/shell.c
@@ -1,4 +1,12 @@
+#include <server/clock.h>
+#include <server/io.h>
+#include <server/shell.h>
+#include <server/trainset.h>
+#include <user/io.h>
+#include <user/clock.h>
+#include <user/name.h>
 #include <user/tasks.h>
+#include <user/trainset.h>
 
 static void shell_parser_task() {
     Exit();
@@ -13,8 +21,21 @@ static void shell_clock_task() {
 }
 
 static void shell_sensor_task() {
+    ActiveTrainSensorList list;
+    int clocktid = WhoIs(CLOCK_SERVER_NAME);
+    int iotid = WhoIs(IO_SERVER_NAME);
+    int traintid = WhoIs(TRAINSET_SERVER_NAME);
+    for (;;) {
+        Delay(clocktid, SENSOR_READ_INTERVAL/10);
+        list = Trainset_Sensor_Readall(traintid);
+        for (uint32_t i = 0; i < list.size; i++) {
+            Printf(iotid, COM2, "%c", list.sensors[i].module);
+            Printf(iotid, COM2, "%d\n\r", list.sensors[i].id);
+        };
+    }
     Exit();
 }
+
 
 void CreateShellServer(uint32_t priority) {
     Create(priority, &shell_parser_task);
