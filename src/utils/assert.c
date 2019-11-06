240f8420345ac3c260fa1594cdfe6441283cfaaa
x77lin <x77lin@nettop169.cs.uwaterloo.ca>
2019-11-06 16:57:17 -0500

    Fix wrong condition in panic() function.

--- src/utils/assert.c
+++ src/utils/assert.c
@@ -4,7 +4,7 @@ void panic(char *expr, char *file, int line) {
     asm("mrs r12, cpsr");
     register uint32_t CPSR asm("r12");
 
-    if (!(CPSR & PSR_MODE_SVC)) {
+    if ((CPSR & 0x1F) == PSR_MODE_USR) {
         SYSCALL_INVOKE(SYSCALL_PANIC);
     } else {
         bwprintf(COM2, "\033[2J\033[1;1H");
