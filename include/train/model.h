375bd4e96352c35075c92378d6bb23fef33f915f
x77lin <x77lin@nettop169.cs.uwaterloo.ca>
2019-11-19 17:13:39 -0500

    Adjust the function for estimate stop distance.

--- include/train/model.h
+++ include/train/model.h
@@ -8,6 +8,6 @@ void model_estimate_train_status(Train *train);
 
 void model_correct_train_status(TrainSensorList *sensorlist);
 
-uint32_t model_estimate_train_stop_distance(Train *train);
+uint32_t model_estimate_train_stop_distance(uint32_t train_id, uint32_t velocity);
 
 #endif /*__TRAIN_MODEL_H__*/
