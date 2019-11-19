e8cfb84321bd22450093eeef7e489826699eb676
x77lin <x77lin@nettop169.cs.uwaterloo.ca>
2019-11-18 22:36:29 -0500

    Add model component to train.

---
+++ include/train/model.h
@@ -0,0 +1,9 @@
+#ifndef __TRAIN_MODEL_H__
+#define __TRAIN_MODEL_H__
+
+#include <stdint.h>
+#include <train/train.h>
+
+void model_estimate_train_status(Train *train);
+
+#endif /*__TRAIN_MODEL_H__*/
---
+++ src/train/model.c
@@ -0,0 +1,52 @@
+#include <hardware/timer.h>
+#include <train/model.h>
+#include <utils/assert.h>
+
+uint32_t acceleration_table[5] = { 137, 132, 124, 195, 125 };
+
+uint32_t expected_velocity_matrix[5][5] = {
+    {340, 400, 470, 530, 600},
+    {350, 410, 480, 550, 630},
+    {320, 380, 450, 520, 600},
+    {490, 550, 600, 630, 630},
+    {270, 330, 390, 440, 500},
+};
+
+static uint32_t expected_acceleration(uint32_t train_id) {
+    return acceleration_table[train_id_to_index(train_id)];
+}
+
+static uint32_t expected_velocity(uint32_t train_id, uint32_t speed) {
+    assert(speed >= 10 && speed <= 14);
+    return expected_velocity_matrix[train_id_to_index(train_id)][speed - 10];
+}
+
+static uint32_t model_integrate_velocity(uint32_t current_vec, uint32_t target_vec, uint32_t acc, uint32_t dt) {
+    if (current_vec == target_vec) return current_vec;
+
+    uint32_t dv = acc * dt / 1000;
+    if (current_vec < target_vec) {
+        uint32_t remain = target_vec - current_vec;
+        return current_vec + (dv < remain ? dv : remain);
+    } else {
+        uint32_t remain = current_vec - target_vec;
+        return current_vec - (dv < remain ? dv : remain);
+    }
+}
+
+void model_estimate_train_status(Train *train) {
+    uint32_t now = (uint32_t) timer_read(TIMER3);
+    uint32_t dt = now - train->last_position_update_time;
+    train->last_position_update_time = now;
+
+    uint32_t acc = expected_acceleration(train->id);
+    uint32_t vec = expected_velocity(train->id, train->speed);
+
+    uint32_t v0 = train->velocity;
+    train->velocity = model_integrate_velocity(v0, vec, acc, dt);
+    uint32_t vt = train->velocity;
+    uint32_t dd = (v0 + vt) * dt / 2;
+    if (train->position.node != NULL) {
+        position_move(&train->position, (int32_t) dd);
+    }
+}
