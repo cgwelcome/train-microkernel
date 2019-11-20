22fe1ddc26307fad328ebc5d8a5c1e3f709fada5
Charles Gao <c39gao@uwaterloo.ca>
2019-11-20 08:37:22 -0500

    Add collision detection

--- src/train/manager.c
+++ src/train/manager.c
@@ -7,7 +7,8 @@
 
 extern Track singleton_track;
 extern Train singleton_trains[TRAIN_COUNT];
-#define TRAILLING_DISTANCE 400
+#define TRAILLING_DISTANCE 700
+#define SWITCH_RESERVATION_DISTANCE 1000
 
 void train_manager_navigate_train(uint32_t train_id, TrackNode *dest, int32_t offset) {
     Train *train = train_find(singleton_trains, train_id);
@@ -23,8 +24,8 @@ void train_manager_navigate_train(uint32_t train_id, TrackNode *dest, int32_t of
     train->destination = destination;
 }
 
-static void train_manager_prepare_ahead(Train *train, uint32_t stop_distance) {
-    TrackPath subpath = path_to_greater_length(&train->path, stop_distance);
+static void train_manager_prepare_ahead(Train *train) {
+    TrackPath subpath = path_to_greater_length(&train->path, SWITCH_RESERVATION_DISTANCE);
     for (uint32_t i = 0; i < subpath.list.size; i++) {
         TrackEdge *edge = edgelist_by_index(&subpath.list, i);
         TrackNode *src = edge->src;
@@ -38,21 +39,25 @@ static void train_manager_look_ahead(Train *train, uint32_t stop_distance) {
     for (size_t i = 0; i < TRAIN_COUNT; i++) {
         Train *other_train = &singleton_trains[i];
         if (!other_train->inited || other_train->id == train->id) continue;
-        TrackPosition position = {
+        TrackPosition head = {
             .node = other_train->position.node,
             .offset = other_train->position.offset - stop_distance - TRAILLING_DISTANCE,
         };
-        if (train_close_to(train, position) != UINT32_MAX) {
-            if (!train->blocked) {
-                train->original_speed = train->speed;
-                train->blocked = true;
+        TrackPosition rear = {
+            .node = other_train->position.node->reverse,
+            .offset = other_train->position.offset + stop_distance + TRAILLING_DISTANCE,
+        };
+        if (train_close_to(train, head) != UINT32_MAX || train_close_to(train, rear) != UINT32_MAX) {
+            /*if (!train->blocked) {*/
+                /*train->original_speed = train->speed;*/
+                /*train->blocked = true;*/
                 controller_speed_one(train->id, 0, 0);
-            }
+            /*}*/
         }
-        else if (train->blocked) {
-            train->blocked = false;
-            controller_speed_one(train->id, train->original_speed, 0);
-        }
+        /*else if (train->blocked) {*/
+            /*train->blocked = false;*/
+            /*controller_speed_one(train->id, train->original_speed, 0);*/
+        /*}*/
     }
 }
 
@@ -63,7 +68,7 @@ void train_manager_issue_directives() {
             uint32_t stop_distance = model_estimate_train_stop_distance(train);
             if (train->trajectory) {
                 path_move(&train->path, train->position.node);
-                train_manager_prepare_ahead(train, stop_distance);
+                train_manager_prepare_ahead(train);
                 TrackPosition position = {
                     .node = train->destination.node,
                     .offset = train->destination.offset - stop_distance,
