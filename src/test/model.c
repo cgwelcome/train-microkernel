ff9b78321ac3bb45e95fceb7b754e2d0adc177c7
x77lin <x77lin@nettop169.cs.uwaterloo.ca>
2019-11-20 00:27:58 -0500

    Update model test.

--- src/test/model.c
+++ src/test/model.c
@@ -10,7 +10,7 @@
 
 #include <string.h>
 
-#define TRAIN_ID    1
+#define TRAIN_ID     1
 #define TRAIN_SPEED 14
 
 extern Track singleton_track;
@@ -39,11 +39,13 @@ int test_build_model(int argc, char **argv) {
     int clocktid = WhoIs(SERVER_NAME_CLOCK);
     int traintid = WhoIs(SERVER_NAME_TRAIN);
 
-    int stopflag = argc > 0 && !strcmp(argv[1], "stop");
+    int stopflag = argc == 2 && !strcmp(argv[1], "stop");
 
     TrainSpeed(traintid, TRAIN_ID, TRAIN_SPEED);
 
     TrainSensorList sensorlist;
+    uint64_t total_dist = 0;
+    uint64_t total_time = 0;
     TrackNode *last_reach_sensor = NULL;
     uint64_t   last_reach_time   = 0;
     for (;;) {
@@ -67,9 +69,14 @@ int test_build_model(int argc, char **argv) {
 
             uint64_t dist = find_dist(last_reach_sensor, reach_sensor);
             uint64_t time = reach_time - last_reach_time;
-            if (dist != (uint64_t) -1 && dist != 0 && time >= 500) {
+            if (dist != (uint64_t) -1 && dist != 0 && time >= 1000) {
+                total_dist += dist;
+                total_time += time;
                 uint32_t sv = (uint32_t) (dist * 100000 / time);
-                Printf(iotid, COM2, "%s\t%s\t%u\t%u\t%u.%02u\r\n", last_reach_sensor->name, reach_sensor->name, (uint32_t) dist, (uint32_t) time, sv / 100, sv % 100);
+                uint32_t tv = (uint32_t) (total_dist * 100000 / total_time);
+                Printf(iotid, COM2, "%s\t%s\t%u\t%u\t%u.%02u\t%u.%02u\r\n",
+                        last_reach_sensor->name, reach_sensor->name,
+                        (uint32_t) dist, (uint32_t) time, sv / 100, sv % 100, tv / 100, tv % 100);
 
                 last_reach_sensor = reach_sensor;
                 last_reach_time   = reach_time;
