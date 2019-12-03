18a012a9d223340fe18737815d0a461712b782f9
x77lin <x77lin@nettop169.cs.uwaterloo.ca>
2019-12-02 23:11:12 -0500

    Finish the taxi system.

--- include/server/taxi.h
+++ include/server/taxi.h
@@ -1,13 +1,36 @@
 #ifndef __SERVER_TAXI_H__
 #define __SERVER_TAXI_H__
 
+#include <train/track.h>
+
 #include <stdint.h>
 
+#define TAXI_TRIP_POOL_SIZE 8
+
 typedef enum {
-    TAXI_SUNRISE,
-    TAXI_SUNSET,
-    TAXI_CUSTOMER,
+    TAXI_NOTIFY_SUNRISE,
+    TAXI_NOTIFY_SUNSET,
+    TAXI_NOTIFY_CUSTOMER,
 } TaxiNotification;
+
+typedef enum {
+    TAXI_STATE_IDLE,
+    TAXI_STATE_PICKING,
+    TAXI_STATE_DROPPING,
+} TaxiState;
+
+typedef struct {
+    char *   pickup_node;
+    uint32_t pickup_offset;
+    char *   dropoff_node;
+    uint32_t dropoff_offset;
+} TaxiTripCandidate;
+
+typedef struct {
+    TaxiState state;
+    TrackPosition pickup;
+    TrackPosition dropoff;
+} TaxiRecord;
 
 void CreateTaxiSystem();
 
--- src/server/taxi.c
+++ src/server/taxi.c
@@ -1,5 +1,7 @@
 #include <kernel.h>
+#include <hardware/timer.h>
 #include <server/taxi.h>
+#include <train/train.h>
 #include <user/clock.h>
 #include <user/ipc.h>
 #include <user/name.h>
@@ -7,23 +9,81 @@
 #include <user/train.h>
 #include <utils/assert.h>
 
-void taxi_system_root_task() {
+extern Track singleton_track;
+
+static TaxiRecord records[TRAIN_COUNT];
+static const TaxiTripCandidate trip_pool[TAXI_TRIP_POOL_SIZE] = {
+    {"B15", 0, "D16", 0},
+    {"C9" , 0, "E7" , 0},
+    {"C5" , 0, "D13", 0},
+    {"D1" , 0, "D11", 0},
+    {"E5" , 0, "E2" , 0},
+    {"C13", 0, "E15", 0},
+    {"D11", 0, "E16", 0},
+    {"E5" , 0, "A3" , 0},
+};
+
+static void taxi_schedule_trip(TaxiRecord *record) {
+    uint64_t now = timer_read_raw(TIMER3);
+    const TaxiTripCandidate *candidate = &trip_pool[now % TAXI_TRIP_POOL_SIZE];
+    record->pickup  = (TrackPosition) {
+        .node   = track_find_node_by_name(&singleton_track, candidate->pickup_node),
+        .offset = candidate->pickup_offset,
+    };
+    record->dropoff = (TrackPosition) {
+        .node   = track_find_node_by_name(&singleton_track, candidate->dropoff_node),
+        .offset = candidate->dropoff_offset,
+    };
+}
+
+static void taxi_check_next_step(int traintid, uint32_t idle_train) {
+    TaxiRecord *record = &records[train_id_to_index(idle_train)];
+    if (record->state == TAXI_STATE_IDLE) {
+        taxi_schedule_trip(record);
+    }
+    switch (record->state) {
+        case TAXI_STATE_IDLE:
+            TrainMove(traintid, idle_train, 10, record->pickup.node, (int32_t) record->pickup.offset);
+            record->state = TAXI_STATE_PICKING;
+            break;
+        case TAXI_STATE_PICKING:
+            TrainMove(traintid, idle_train, 10, record->dropoff.node, (int32_t) record->dropoff.offset);
+            record->state = TAXI_STATE_DROPPING;
+            break;
+        case TAXI_STATE_DROPPING:
+            record->state = TAXI_STATE_IDLE;
+            break;
+        default:
+            throw("unknown state");
+    }
+}
+
+static void taxi_system_root_task() {
     int tid;
     TaxiNotification notification;
 
     RegisterAs(SERVER_NAME_TAXI);
     int traintid = WhoIs(SERVER_NAME_TRAIN);
+    for (int i = 0; i < TRAIN_COUNT; i++) {
+        records[i].state = TAXI_STATE_IDLE;
+    }
     for (;;) {
         Receive(&tid, (char *)&notification, sizeof(notification));
         Reply(tid, NULL, 0);
         switch (notification) {
-            case TAXI_SUNRISE:
+            case TAXI_NOTIFY_SUNRISE:
                 TrainLight(traintid, false);
                 break;
-            case TAXI_SUNSET:
+            case TAXI_NOTIFY_SUNSET:
                 TrainLight(traintid, true);
                 break;
-            case TAXI_CUSTOMER:
+            case TAXI_NOTIFY_CUSTOMER:
+                if (singleton_track.inited) {
+                    uint32_t idle_train = TrainFindIdle(traintid);
+                    if (idle_train != 0) {
+                        taxi_check_next_step(traintid, idle_train);
+                    }
+                }
                 break;
             default:
                 throw("unknown request");
@@ -32,32 +92,32 @@ void taxi_system_root_task() {
     Exit();
 }
 
-void taxi_system_notifier_suntime() {
+static void taxi_system_notifier_suntime() {
     int clocktid = WhoIs(SERVER_NAME_CLOCK);
     int taxitid  = WhoIs(SERVER_NAME_TAXI);
 
-    TaxiNotification suntime_notification = TAXI_SUNRISE;
+    TaxiNotification suntime_notification = TAXI_NOTIFY_SUNRISE;
 
     for (;;) {
         Delay(clocktid, 18000); // 3 minutes
-        if (suntime_notification == TAXI_SUNRISE) {
-            suntime_notification = TAXI_SUNSET;
+        if (suntime_notification == TAXI_NOTIFY_SUNRISE) {
+            suntime_notification = TAXI_NOTIFY_SUNSET;
         } else {
-            suntime_notification = TAXI_SUNRISE;
+            suntime_notification = TAXI_NOTIFY_SUNRISE;
         }
         assert(Send(taxitid, (char *)&suntime_notification, sizeof(suntime_notification), NULL, 0) >= 0);
     }
     Exit();
 }
 
-void taxi_system_notifier_customer() {
+static void taxi_system_notifier_customer() {
     int clocktid = WhoIs(SERVER_NAME_CLOCK);
     int taxitid  = WhoIs(SERVER_NAME_TAXI);
 
-    TaxiNotification customer_notification = TAXI_CUSTOMER;
+    TaxiNotification customer_notification = TAXI_NOTIFY_CUSTOMER;
 
     for (;;) {
-        Delay(clocktid, 500); // 5 seconds
+        Delay(clocktid, 1000); // 10 seconds
         assert(Send(taxitid, (char *)&customer_notification, sizeof(customer_notification), NULL, 0) >= 0);
     }
     Exit();
